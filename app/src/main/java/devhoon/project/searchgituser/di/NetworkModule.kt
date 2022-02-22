bca34d296570a9354cc50e9552d6668d85dd89f7
DongHoonKim <welcome2c@mobidoo.co.kr>
2022-02-22 18:49:39 +0900

    network module 수정

--- app/src/main/java/devhoon/project/searchgituser/di/NetworkModule.kt
+++ app/src/main/java/devhoon/project/searchgituser/di/NetworkModule.kt
@@ -1,3 +1,42 @@
 package devhoon.project.searchgituser.di
 
+import devhoon.project.searchgituser.data.GitHubApi
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 const val TIME_OUT = 10L
+
+val httpLoggingModule = module {
+    single <Interceptor> {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+}
+
+val httpClientModule = module {
+    single {
+        OkHttpClient.Builder()
+            .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .readTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .writeTimeout(TIME_OUT, TimeUnit.SECONDS)
+            .addInterceptor(get<Interceptor>())
+            .build()
+    }
+}
+
+val githubApiModule = module {
+    single {
+        Retrofit.Builder()
+            .baseUrl(GitHubApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+            .create(GitHubApi::class.java)
+    }
+}
