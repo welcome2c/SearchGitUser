ebf88b6f2b177395bdedc54c6378023eeee44d2b
DevHoon <welcome2c@mobidoo.co.kr>
2022-02-22 17:52:11 +0900

    MainActivity Base상속받도록 수정

--- app/src/main/java/devhoon/project/searchgituser/ui/main/MainActivity.kt
+++ app/src/main/java/devhoon/project/searchgituser/ui/main/MainActivity.kt
@@ -3,10 +3,11 @@ package devhoon.project.searchgituser.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import devhoon.project.searchgituser.R
+import devhoon.project.searchgituser.databinding.ActivityMainBinding
+import devhoon.project.searchgituser.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
