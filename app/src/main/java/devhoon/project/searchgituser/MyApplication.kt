e74e7b74643170a384074c7e1aabe8c0b393cd31
DongHoonKim <welcome2c@mobidoo.co.kr>
2022-02-23 10:33:28 +0900

    코인 적용

--- app/src/main/java/devhoon/project/searchgituser/MyApplication.kt
+++ app/src/main/java/devhoon/project/searchgituser/MyApplication.kt
@@ -1,9 +1,21 @@
 package devhoon.project.searchgituser
 
 import android.app.Application
+import org.koin.core.context.startKoin
+import devhoon.project.searchgituser.di.*
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            modules(
+                listOf(
+                    githubApiModule,
+                    repositoryModule,
+                    viewModelModule
+                )
+            )
+        }
     }
 }
