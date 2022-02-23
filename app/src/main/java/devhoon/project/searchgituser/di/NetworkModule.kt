1f2c6d0be6a8a7c4e7d655e6325fae9e6e93c302
DongHoonKim <welcome2c@mobidoo.co.kr>
2022-02-23 10:30:19 +0900

    Api 통신 관련 코드 작성

--- app/src/main/java/devhoon/project/searchgituser/di/NetworkModule.kt
+++ app/src/main/java/devhoon/project/searchgituser/di/NetworkModule.kt
@@ -1,8 +1,10 @@
 package devhoon.project.searchgituser.di
 
+import android.util.Log
 import devhoon.project.searchgituser.data.GitHubApi
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -11,11 +13,22 @@ import java.util.concurrent.TimeUnit
 
 const val TIME_OUT = 10L
 
-val httpLoggingModule = module {
-    single <Interceptor> {
-        HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
+val debugInterceptor = HttpLoggingInterceptor(object: HttpLoggingInterceptor.Logger {
+    override fun log(message: String) {
+        Log.d("API", message)
+    }
+}).apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
+
+val authInterceptor = object : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        val builder = request.newBuilder()
+        builder.addHeader("Authorization", "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")
+        request = builder.build()
+
+        return chain.proceed(request)
     }
 }
 
@@ -25,7 +38,8 @@ val httpClientModule = module {
             .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
             .readTimeout(TIME_OUT, TimeUnit.SECONDS)
             .writeTimeout(TIME_OUT, TimeUnit.SECONDS)
-            .addInterceptor(get<Interceptor>())
+            .addInterceptor(debugInterceptor)
+            .addInterceptor(authInterceptor)
             .build()
     }
 }
