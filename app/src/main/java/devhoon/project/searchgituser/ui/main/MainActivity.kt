6fb0e6b4cec4233b397c9ea3f737e371dde18dae
DevHoon <welcome2c@mobidoo.co.kr>
2022-02-23 17:03:30 +0900

    Navigation component 적용

--- app/src/main/java/devhoon/project/searchgituser/ui/main/MainActivity.kt
+++ app/src/main/java/devhoon/project/searchgituser/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package devhoon.project.searchgituser.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import devhoon.project.searchgituser.R
 import devhoon.project.searchgituser.databinding.ActivityMainBinding
 import devhoon.project.searchgituser.ui.base.BaseActivity
@@ -9,8 +11,18 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private val mainViewModel: MainViewModel by viewModel()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding {
+            mainVm = mainViewModel
+            navController = navHostFragment.navController
+            bottomNavigation.setupWithNavController(navController)
+        }
     }
 }
