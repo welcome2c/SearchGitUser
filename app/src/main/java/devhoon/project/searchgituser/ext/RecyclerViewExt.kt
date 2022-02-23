413a2496ff015e732059b3fbca3626d11c7639bf
DongHoonKim <welcome2c@mobidoo.co.kr>
2022-02-23 14:16:39 +0900

    RecyclerView Ext 추가

---
+++ app/src/main/java/devhoon/project/searchgituser/ext/RecyclerViewExt.kt
@@ -0,0 +1,16 @@
+package devhoon.project.searchgituser.ext
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import devhoon.project.searchgituser.ui.main.UserListAdapter
+
+@BindingAdapter("android:setItems")
+fun <T> RecyclerView.setItems(items: List<T>?) {
+    items?.let {
+        when (adapter) {
+            is UserListAdapter -> {
+                (adapter as UserListAdapter).submitList(items as List<Item>)
+            }
+        }
+    }
+}
